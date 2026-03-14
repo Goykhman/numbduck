8fca13ca327810ecff4fbd6f09dd21b0db3ab8b3
Nelson, Erik <erik_lee_nelson@yahoo.com>
2026-03-13 20:19:11 -0400

    Simplify array_data_p: use ctypes.data with intp cast instead of @intrinsic
    Replace the LLVM @intrinsic (_array_data_p) with a simple @njit function
    that uses arr.ctypes.data (available in numba nopython mode) and casts
    from uint64 to intp to match numbox signature conventions.

--- numbduck/jit_utils.py
+++ numbduck/jit_utils.py
@@ -2,20 +2,16 @@
 # in the future.
 from numba import njit
 from numba.core.types import intp
-from numba.extending import intrinsic
-from numba.np.arrayobj import make_array
-
-
-@intrinsic
-def _array_data_p(typingctx, arr_ty):
-    """Extract the data pointer from a numpy array as an intp."""
-    def codegen(context, builder, signature, arguments):
-        arr_struct = make_array(arr_ty)(context, builder, arguments[0])
-        return builder.ptrtoint(arr_struct.data, context.get_data_type(intp))
-    return intp(arr_ty), codegen
 
 
 @njit
 def array_data_p(arr):
-    """Return the data pointer of a numpy array. Callable from Python and @njit."""
-    return _array_data_p(arr)
+    """Return the data pointer of a numpy array as intp.
+
+    Wraps arr.ctypes.data (which returns uint64 in numba) with an intp cast
+    so the result matches the signed-integer pointer convention used by numbox
+    binding signatures.
+
+    Callable from both Python and @njit context.
+    """
+    return intp(arr.ctypes.data)
